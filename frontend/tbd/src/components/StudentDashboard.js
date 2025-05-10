050302f1649a0109fa1f91c89559f8b36874f200
merc-cyy <mercymuiruri2027@u.northwestern.edu>
2025-05-09 23:02:53 -0500

    Add

--- frontend/tbd/src/components/StudentDashboard.js
+++ frontend/tbd/src/components/StudentDashboard.js
@@ -4,9 +4,9 @@ import { useEffect } from 'react';
 
 function StudentDashboard() {
     // Mock data for interested posts - replace with actual data fetching
-    //const profileData = JSON.parse(localStorage.getItem('profileData'));
-    // const firstName = profileData.first_name
-    // const lastName = profileData.last_name
+    const profileData = JSON.parse(localStorage.getItem('profileData'));
+    const firstName = profileData.first_name
+    const lastName = profileData.last_name
 
    // const backendApiUrl = 'https://raiso-ieee-2025.onrender.com';
     const backendApiUrl ='https://backend-ieee.onrender.com';
@@ -116,7 +116,7 @@ function StudentDashboard() {
                             <i class="bi bi-person-fill fs-1 custom-icon-color"></i>
                         </div>
                     <div>
-                    <h5>{firstname} {lastname}</h5>
+                    <h5>{firstName} {lastName}</h5>
                     {/* Optional: User Role */}
                     <p className="text-muted small">Student</p>
                             </div>
